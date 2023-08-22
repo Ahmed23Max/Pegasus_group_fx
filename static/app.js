53d10c70d71bf9cae299f98944140f84f58b5efc
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-08-22 11:44:22 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,1 +1,13 @@
+$(document).ready(function () {
+    $(".navbar-toggler").click(function () {
+        $(".mobile-overlay").fadeToggle(); // Toggle the overlay
+        $("body").toggleClass("no-scroll"); // Prevent scrolling on background
+    });
+
+    // Close the overlay when clicking anywhere on it
+    $(".mobile-overlay").click(function () {
+        $(this).fadeOut();
+        $("body").removeClass("no-scroll");
+    });
+});
 
