866eed24a3f1aab11ebaa49c5f70916db6b27a46
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-08-22 11:11:04 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,18 +1,1 @@
- document.addEventListener("DOMContentLoaded", function () {
-        const blurBackground = document.querySelector(".blur-background");
-        const fullScreenNavbar = document.querySelector(".full-screen-navbar");
-        const mainNavbarToggler = document.querySelector(".navbar-toggler");
-        const mobileNavbarToggler = document.querySelector(".navbar-content .navbar-toggler");
-        
-        mainNavbarToggler.addEventListener("click", function () {
-            blurBackground.classList.toggle("show-blur");
-            fullScreenNavbar.classList.toggle("show-navbar");
-            document.body.style.overflow = "hidden";
-        });
-        
-        mobileNavbarToggler.addEventListener("click", function () {
-            blurBackground.classList.remove("show-blur");
-            fullScreenNavbar.classList.remove("show-navbar");
-            document.body.style.overflow = "auto";
-        });
-    });
+
