0526fe63e4974fdaa0d7ebc8363961f8533c56c9
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-08-22 10:19:31 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,18 +1,18 @@
- document.addEventListener("DOMContentLoaded", function () {
-            const blurBackground = document.querySelector(".blur-background");
-            const fullScreenNavbar = document.querySelector(".full-screen-navbar");
-            const navbarToggler = document.querySelector(".navbar-toggler");
-            const navbarTogglerMobile = document.querySelector(".navbar-content .navbar-toggler");
-            
-            navbarToggler.addEventListener("click", function () {
-                blurBackground.classList.toggle("show-blur");
-                fullScreenNavbar.classList.toggle("show-navbar");
-                document.body.style.overflow = "hidden";
-            });
-            
-            navbarTogglerMobile.addEventListener("click", function () {
-                blurBackground.classList.remove("show-blur");
-                fullScreenNavbar.classList.remove("show-navbar");
-                document.body.style.overflow = "auto";
-            });
-        });
+document.addEventListener("DOMContentLoaded", function () {
+    const blurBackground = document.querySelector(".blur-background");
+    const fullScreenNavbar = document.querySelector(".full-screen-navbar");
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarTogglerMobile = document.querySelector(".navbar-content .navbar-toggler");
+    
+    navbarToggler.addEventListener("click", function () {
+        blurBackground.classList.toggle("show-blur");
+        fullScreenNavbar.classList.toggle("show-navbar");
+        document.body.style.overflow = "hidden";
+    });
+    
+    navbarTogglerMobile.addEventListener("click", function () {
+        blurBackground.classList.remove("show-blur");
+        fullScreenNavbar.classList.remove("show-navbar");
+        document.body.style.overflow = "auto";
+    });
+});
