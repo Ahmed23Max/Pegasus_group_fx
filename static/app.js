17acc083a86237b619c6180b595651d655f679a5
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-08-22 10:54:17 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,20 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const blurBackground = document.querySelector(".blur-background");
-    const fullScreenNavbar = document.querySelector(".full-screen-navbar");
-    const navbarToggler = document.querySelector(".navbar-toggler");
-    const navbarTogglerMobile = document.querySelector(".navbar-content .navbar-toggler");
-    
-    navbarToggler.addEventListener("click", function () {
-        toggleFullScreenNavbar();
+ document.addEventListener("DOMContentLoaded", function () {
+        const blurBackground = document.querySelector(".blur-background");
+        const fullScreenNavbar = document.querySelector(".full-screen-navbar");
+        const mainNavbarToggler = document.querySelector(".navbar-toggler");
+        const mobileNavbarToggler = document.querySelector(".navbar-content .navbar-toggler");
+        
+        mainNavbarToggler.addEventListener("click", function () {
+            blurBackground.classList.toggle("show-blur");
+            fullScreenNavbar.classList.toggle("show-navbar");
+            document.body.style.overflow = "hidden";
+        });
+        
+        mobileNavbarToggler.addEventListener("click", function () {
+            blurBackground.classList.remove("show-blur");
+            fullScreenNavbar.classList.remove("show-navbar");
+            document.body.style.overflow = "auto";
+        });
     });
-    
-    navbarTogglerMobile.addEventListener("click", function () {
-        toggleFullScreenNavbar();
-    });
-    
-    function toggleFullScreenNavbar() {
-        blurBackground.classList.toggle("show-blur");
-        fullScreenNavbar.classList.toggle("show-navbar");
-        document.body.style.overflow = fullScreenNavbar.classList.contains("show-navbar") ? "hidden" : "auto";
-    }
-});
