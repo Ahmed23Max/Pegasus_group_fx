1fb18f11b40056eb5bc284417a14ece1c0e6dffa
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-09-12 11:29:42 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,18 +1,1 @@
-// welcome.js
 
-// Check if a flag is set in session storage
-if (!sessionStorage.getItem('visited_welcome_page')) {
-    // If not set, it means the user hasn't seen the welcoming page before
-    // Perform the animation and set the flag
-    setTimeout(function () {
-        // Redirect to the homepage (replace 'home.html' with your homepage URL)
-        window.location.href = 'home.html';
-    }, 5000); // 5000 milliseconds (5 seconds) delay before redirecting
-
-    // Set the flag in session storage to indicate that the user has visited the welcoming page
-    sessionStorage.setItem('visited_welcome_page', 'true');
-} else {
-    // If the flag is set, the user has already seen the welcoming page
-    // Redirect to the homepage immediately
-    window.location.href = 'home.html';
-}
