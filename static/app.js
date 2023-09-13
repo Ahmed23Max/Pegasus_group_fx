eaae34502648fa57b8e25b1c951790d4d3856ac0
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-09-13 09:31:04 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,14 +1,6 @@
 // JavaScript to toggle between displaying profile and edit form
-document.getElementById("editDateOfBirth").addEventListener("click", function () {
-    toggleEditForm("dateOfBirth");
-});
-
-document.getElementById("editLocation").addEventListener("click", function () {
-    toggleEditForm("location");
-});
-
-document.getElementById("editPhoneNumber").addEventListener("click", function () {
-    toggleEditForm("phoneNumber");
+document.getElementById("editProfile").addEventListener("click", function () {
+    toggleEditForm();
 });
 
 document.getElementById("cancelEdit").addEventListener("click", function () {
@@ -21,23 +13,13 @@ document.getElementById("saveChanges").addEventListener("click", function () {
     toggleEditForm();
 });
 
-function toggleEditForm(fieldToEdit = "") {
+function toggleEditForm() {
     const card = document.querySelector(".card");
     const editForm = document.getElementById("editForm");
     const editButtons = document.getElementById("editButtons");
 
-    if (fieldToEdit) {
-        // Display the edit form for the selected field
-        const fieldElement = document.getElementById(fieldToEdit);
-        const fieldInput = document.getElementById(fieldToEdit.toLowerCase());
-        fieldInput.value = fieldElement.innerText.trim();
-        card.style.display = "none";
-        editForm.style.display = "block";
-        editButtons.style.display = "block";
-    } else {
-        // Display the profile card and hide the edit form and buttons
-        card.style.display = "block";
-        editForm.style.display = "none";
-        editButtons.style.display = "none";
-    }
+    // Display the profile card and hide the edit form and buttons
+    card.style.display = "block";
+    editForm.style.display = "none";
+    editButtons.style.display = "none";
 }
