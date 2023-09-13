5776a0f09d83cb6311cc5998f7d16d5afa7761ee
Ahmed23Max <92646269+Ahmed23Max@users.noreply.github.com>
2023-09-13 10:08:13 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const editProfileButton = document.getElementById("editProfile");
     const cancelEditButton = document.getElementById("cancelEdit");
     const saveChangesButton = document.getElementById("saveChanges");
+    const profileForm = document.getElementById("profileForm");
 
     editProfileButton.addEventListener("click", function () {
         toggleEditForm();
@@ -13,9 +14,34 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     saveChangesButton.addEventListener("click", function () {
-        // Perform AJAX request to update profile data
-        // Update the displayed data
-        toggleEditForm();
+        // Serialize the form data into a JSON object
+        const formData = new FormData(profileForm);
+        const data = {};
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+
+        // Send an AJAX request to update the profile data
+        fetch("/update_profile", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            if (result.message === "Profile updated successfully!") {
+                // Profile updated successfully, toggle the form
+                toggleEditForm();
+            } else {
+                // Handle errors or display a message to the user
+                console.error("Profile update failed:", result.message);
+            }
+        })
+        .catch((error) => {
+            console.error("An error occurred:", error);
+        });
     });
 
     function toggleEditForm() {
